f57ef3bd2c0e22165fa99a614ef045917f63199c
Joshua Byrd <phocks@gmail.com>
2020-07-22 22:14:27 +1000

    Maybe Promises are good

--- main.ts
+++ main.ts
@@ -7,25 +7,15 @@ config({
   export: true,
 });
 
+// Create a new twitter client
 const client = new twitter({
   consumer_key: Deno.env.get("CONSUMER_KEY"),
   consumer_secret: Deno.env.get("CONSUMER_SECRET"),
   bearer_token: Deno.env.get("BEARER_TOKEN"),
 });
 
-const params = {
-  q: "#EStreamerCoders -filter:retweets",
-  tweet_mode: "extended",
-};
-
-client.get("search/tweets", params, function (
-  error: any,
-  tweets: any,
-  response: any
-) {
-  if (!error) {
-    console.log(tweets);
-  } else {
-    console.error(error);
-  }
-});
+client
+  .get("favorites/list", {
+    screen_name: "phocks",
+  })
+  ?.then((response) => console.log(response));
